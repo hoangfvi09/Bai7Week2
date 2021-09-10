756e5c2a14f288abb65cf6619e4b27864c5791d6
hoangfvi09 <huonglt129@gmail.com>
2021-09-10 13:41:18 +0700

    them bai tap

---
+++ ThucHanh_SuDungToanTu.js
@@ -0,0 +1,9 @@
+
+let Width;
+let Height;
+Width = prompt("Enter the width");
+Height = prompt("Enter the height");
+parseInt(Width);
+parseInt(Height);
+let area = Width * Height;
+document.write("The area is: " + area);
